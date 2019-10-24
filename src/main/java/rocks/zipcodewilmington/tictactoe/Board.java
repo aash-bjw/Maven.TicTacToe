f0c4131410099bd3f58f39bac743f1547ad9e2ca
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-10-24 15:10:12 -0400

    All tests passing

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,65 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character [][] board;
+
     public Board(Character[][] matrix) {
+        this.board = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+
+        if (checkDiagonal('X') || checkHorizontal('X') || checkVertical('X')) {
+            return true;
+        } return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if (checkDiagonal('O') || checkHorizontal('O') || checkVertical('O')) {
+            return true;
+        } return false;
     }
 
     public Boolean isTie() {
-        return null;
+
+        if(!isInFavorOfO() && !isInFavorOfX()){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX()) {
+            return "X";
+        }
+        if(isInFavorOfO()) {
+            return "O";
+        }
+            return "";
+        }
+
+
+    public boolean checkVertical(Character xO){
+        for(int i = 0; i <= 2; i++){
+            if ((board[0][i] == xO) && (board[1][i] == xO) && (board[2][i] == xO)){
+                return true;
+            }
+        } return false;
+    }
+    public boolean checkHorizontal(Character xO){
+        for(int i = 0; i <= 2; i++){
+            if ((board[i][0] == xO) && (board[i][1] == xO) && (board[i][2] == xO)){
+                return true;
+            }
+        } return false;
+    }
+
+    public boolean checkDiagonal(Character xO){
+        if ((board[0][0] == xO && board[1][1] == xO  && board[2][2] == xO) ||
+                (board[2][0] == xO && board [1][1] == xO && board[0][2] == xO)){
+            return true;
+        } return false;
     }
 
 }
